f00fe9ce501de2001dd5bc994371e0acd9309815
rbhriegu459 <rbhriegu459@gmail.com>
2023-11-21 22:56:03 +0530

    Object-local-Storage

--- main.js
+++ main.js
@@ -17,15 +17,28 @@ function onsubmit(e){
     } 
     
     else{
+        // // Adding the details in the array element
         // var d =  {name, email};
         // data.push(d);
         // console.log(data);
 
+        // //Adding values as a list item and displaying them
         // const li = document.createElement('li');
         // li.appendChild(document.createTextNode(`${name} : ${email}`));
         // user.appendChild(li);
 
-        localStorage.setItem(name, email);
-        console.log(localStorage.getItem(name));
+        // // Storing the values in local storage as key value pair
+        // localStorage.setItem(name, email);
+        // console.log(localStorage.getItem(name));
+
+        // Storing the values in local storage as object 
+        let obj = {
+            "name" : name,
+            "email" : email,
+        };
+        let obj_serailized = JSON.stringify(obj);
+
+        localStorage.setItem("obj",obj_serailized);
+        console.log(localStorage)
     }
 };
