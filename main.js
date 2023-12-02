76204a5beaab7f459afe1e8d5e31108ff78cc2c7
rbhriegu459 <rbhriegu459@gmail.com>
2023-12-02 13:31:17 +0530

    crudcrudAdded

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const sub = document.querySelector('.btn');
 const user = document.querySelector('#users');
-var data = new Array();
 
 sub.addEventListener('click', onsubmit);
+
+// axios.get('https://crudcrud.com/api/1c1f911c9bb64573857d6f80f1bde3f0/BookingData')
+//     .then(res => {
+//         showOutput(res);
+//     })
+//     .catch(err => console.log(err));
 
 function onsubmit(e){
     e.preventDefault();
@@ -17,61 +22,74 @@ function onsubmit(e){
     } 
     
     else{
-        // // Adding the details in the array element
-        // var d =  {name, email};
-        // data.push(d);
-        // console.log(data);
+    //     // // Adding the details in the array element
+    //     // var d =  {name, email};
+    //     // data.push(d);
+    //     // console.log(data);
 
-        // //Adding values as a list item and displaying them
-        // const li = document.createElement('li');
-        // li.appendChild(document.createTextNode(`${name} : ${email}`));
-        // user.appendChild(li);
+    //     // //Adding values as a list item and displaying them
+    //     // const li = document.createElement('li');
+    //     // li.appendChild(document.createTextNode(`${name} : ${email}`));
+    //     // user.appendChild(li);
 
-        // // Storing the values in local storage as key value pair
-        // localStorage.setItem(name, email);
-        // console.log(localStorage.getItem(name));
+    //     // // Storing the values in local storage as key value pair
+    //     // localStorage.setItem(name, email);
+    //     // console.log(localStorage.getItem(name));
 
         // Storing the values in local storage as object 
         let obj = {
             "name" : name,
             "email" : email,
         };
-        let obj_serailized = JSON.stringify(obj);
 
-        localStorage.setItem(obj.email ,obj_serailized);
+        // Storing the data to crudcrud network
 
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${obj.name} : ${obj.email}`));
-        li.id = obj.email;
-        user.appendChild(li);
+        axios.post('https://crudcrud.com/api/1c1f911c9bb64573857d6f80f1bde3f0/BookingData', obj)
+            .then(res => {
+                showOutput(obj);
+            })
+            .catch(err => console.log(err));
 
-        // Adding a delete button for deleting the details
-        const addBtn = document.createElement('button');
-        addBtn.className = "delBtn";
-        addBtn.appendChild(document.createTextNode("Delete"));
-        li.appendChild(addBtn);
+    //     let obj_serailized = JSON.stringify(obj);
 
-        addBtn.addEventListener('click', (e) =>{
-            e.preventDefault();
-            localStorage.removeItem(email);
-            user.removeChild(document.getElementById(obj.email));
-        });
+    //     localStorage.setItem(obj.email ,obj_serailized);
+            // showOutput(obj);
 
-        // Adding a edit button for editing the details
-        const addEditBtn = document.createElement('button');
-        addEditBtn.className = "editBtn";
-        addEditBtn.appendChild(document.createTextNode("Edit"));
-        li.appendChild(addEditBtn);
-
-        addEditBtn.addEventListener('click', (e) =>{
-            // popullating the values to the input-
-            name.value = obj.name;
-            email.value = obj.email;
-
-            // removing the element from storage and screen-
-            e.preventDefault();
-            localStorage.removeItem(email);
-            user.removeChild(document.getElementById(obj.email));
-        });
     }
 };
+
+function showOutput(obj){
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${obj.name} : ${obj.email}`));
+    li.id = obj.email;
+    user.appendChild(li);
+
+    // Adding a delete button for deleting the details
+    const addBtn = document.createElement('button');
+    addBtn.className = "delBtn";
+    addBtn.appendChild(document.createTextNode("Delete"));
+    li.appendChild(addBtn);
+
+    // Adding a edit button for editing the details
+    const addEditBtn = document.createElement('button');
+    addEditBtn.className = "editBtn";
+    addEditBtn.appendChild(document.createTextNode("Edit"));
+    li.appendChild(addEditBtn);
+
+    addBtn.addEventListener('click', (e) =>{
+                e.preventDefault();
+                localStorage.removeItem(email);
+                user.removeChild(document.getElementById(obj.email));
+            });
+    
+    addEditBtn.addEventListener('click', (e) =>{
+        // popullating the values to the input-
+        name.value = obj.name;
+        email.value = obj.email;
+    
+        // removing the element from storage and screen-
+        e.preventDefault();
+        localStorage.removeItem(email);
+        user.removeChild(document.getElementById(obj.email));
+    });
+}
